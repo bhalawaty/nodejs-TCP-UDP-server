69f29db39788b806eacb6dc69b7fa2701e20a231
bhalawaty <bilal.halawaty@gmail.com>
2021-04-24 20:38:45 +0200

    tcp server

---
+++ tcp.js
@@ -0,0 +1,10 @@
+const net = require("net")
+
+const server = net.createServer(socket => {
+    socket.write("Hello bilal.")
+    socket.on("data", data => {
+        console.log(data.toString())
+    })
+})
+
+server.listen(8090)
